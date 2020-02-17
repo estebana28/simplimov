7b8be72d5c4d9c243a5afdbc980ceb04b29d8c26
estebana28 <eesteban.arce@gmail.com>
2020-02-17 14:47:55 -0300

    page fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home.js';
-import Bike from './pages/Bike.js';
 
 import './App.css';
 
@@ -12,7 +11,6 @@ function App() {
     <HashRouter basename="/">
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/bike/:slug" component={Bike} />
           <Route component={Error} />
         </Switch>
     </HashRouter>
