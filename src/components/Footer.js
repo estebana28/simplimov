9aa0eff7c259cf5b1ab4de8bc802e86dd3490191
eesteban.arce <eesteban.arce@gmail.com>
2020-02-17 09:32:23 -0300

    footer hooks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,51 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import fb_img from '../images/facebook.svg';
-import ln_img from '../images/linkedin.svg';
-import tw_img from '../images/twitter.svg';
-import yt_img from '../images/youtube.svg';
-import ins_img from '../images/instagram.svg';
+// import fb_img from '../images/facebook.svg';
+// import ln_img from '../images/linkedin.svg';
+// import tw_img from '../images/twitter.svg';
+// import yt_img from '../images/youtube.svg';
+// import ins_img from '../images/instagram.svg';
 
 
 
-export default class Header extends React.Component {
-  state = {
-    config: []
-  }
+const Footer = () => {
+  const [contact, setContact] = useState(undefined);
+  const [social, setSocial] = useState(undefined);
+  const [error, setError] = useState(false);
 
-  componentDidMount() {
+  useEffect(() => {
     axios.get('https://api.myjson.com/bins/17dcjc')
       .then(res => {
-        const config = res.data;
-        this.setState({ config });
+        setContact(res.data.contact);
+        setSocial(res.data.social);
       })
-  }
 
-  render() {
-    const { contact: {tel, email, address} = {}, social: {fb, ln, tw, yt, ins} = {} } = this.state.config;
+      .catch(err => setError(err));
+  }, []);
+
+  console.log("contact",contact);
+
+  if (error) {
+    return (
+      <div>
+        <p>Ups, tuvimos un problema para cargar los datos del footer. Intentelo nuevamente mas tarde</p>
+      </div>
+    );
+  } 
+
+
+
     return (
       <footer>
-        <div>
-          <h3>Contacto</h3>
-          <h4>{tel}</h4>
-          <h4>{email}</h4>
-          <h4>{address}</h4>
-        </div>
+        {contact ? <div> Cargando... </div>:
+          <div>
+            <h3>Contacto</h3>
+            <h4>{contact.tel}</h4>
+            <h4>{}</h4>
+            <h4>{}</h4>
+          </div>}
+        {/* 
         <div>
           <h3>Nuestras Redes</h3>
           <ul className="social-links">
@@ -41,8 +55,9 @@ export default class Header extends React.Component {
             <li><a href={yt} target="_blank" rel="noopener noreferrer"><img src={yt_img} alt=""/></a></li>
             <li><a href={ins} target="_blank" rel="noopener noreferrer"><img src={ins_img} alt=""/></a></li>
           </ul>
-        </div>
+        </div> */}
       </footer>
     )
   }
-}
+
+export default Footer;
